a46f3587f7187dc738f9926b2c5745eab9ebcbf1
Willian Justen de Vasconcellos <willianjustenqui@gmail.com>
2022-01-17 16:52:49 -0300

    Add scaffolding

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,58 +1,16 @@
-import Head from 'next/head'
-import Block from 'components/Block'
-import Footer from 'components/Footer'
-
 export default function Home() {
   return (
-    <div className="flex flex-col justify-center items-center py-2 min-h-screen">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className="flex flex-col flex-1 justify-center items-center px-20 w-full text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
-            pages/index.tsx
-          </code>
-        </p>
-
-        <div className="flex flex-wrap justify-around items-center mt-6 max-w-4xl sm:w-full">
-          <Block
-            href="https://nextjs.org/docs"
-            title="Documentation"
-            description="Find in-depth information about Next.js features and API."
-          />
-
-          <Block
-            href="https://nextjs.org/learn"
-            title="Learn"
-            description="Learn about Next.js in an interactive course with quizzes!"
-          />
-
-          <Block
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            title="Examples"
-            description="Discover and deploy boilerplate example Next.js projects."
-          />
-
-          <Block
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            title="Deploy"
-            description="Instantly deploy your Next.js site to a public URL with Vercel."
-          />
+    <>
+      <div className="flex flex-col bg-gray-800 w-60">
+        <div className="flex items-center h-12 px-3 font-semibold text-white shadow-md font-title">
+          Dashboard
         </div>
-      </main>
+        <div className="flex-1 p-3 space-y-2 overflow-y-scroll font-medium text-gray-300 ">
+          <p className="text-white">Friends</p>
+        </div>
+      </div>
 
-      <Footer />
-    </div>
-  )
+      <div className="flex flex-col flex-1 bg-gray-700"></div>
+    </>
+  );
 }
