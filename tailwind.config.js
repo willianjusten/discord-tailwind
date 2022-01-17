83dfad792c66f9aea3e090d38686146fea27f110
Willian Justen de Vasconcellos <willianjustenqui@gmail.com>
2022-01-17 16:32:56 -0300

    Update dev xp config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,6 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
   mode: 'jit',
-  darkMode: false,
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx}',
     './src/components/**/*.{js,ts,jsx,tsx}'
